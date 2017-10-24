b10ef4656f95d775818c157ef64794f18dc9faeb
srsok <srsok3@gmail.com>
2017-10-24 16:02:31 +0900

    Init AiTES IoT gateway project

---
+++ IoTgateway/Java/AiTESIoTgateway/src/com/github/aites/iotgateway/IoTgateway.java
@@ -0,0 +1,10 @@
+package com.github.aites.iotgateway;
+
+public class IoTgateway {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("IoT gateway Started");
+	}
+
+}
