01b4bdd71bfba923580082922f8c9dfd244be370
srsok <srsok3@gmail.com>
2017-10-24 17:12:54 +0900

    10월 24일 IoTgateway 프로그램 개발 [초기 스텝 구현]

--- IoTgateway/Java/AiTESIoTgateway/src/com/github/aites/iotgateway/IoTgateway.java
+++ IoTgateway/Java/AiTESIoTgateway/src/com/github/aites/iotgateway/IoTgateway.java
@@ -3,8 +3,28 @@ package com.github.aites.iotgateway;
 public class IoTgateway {
 
 	public static void main(String[] args) {
+		
+		
+		if(args[0].equals("-t")){
+			System.out.println("Test mode run");
+		}
+		else if(args[0].equals("-r")){
+			System.out.println("Regular mode run");
+		}
+		PropReader prop = new PropReader();
+		
 		// TODO Auto-generated method stub
-		System.out.println("IoT gateway Started");
+	
+		prop.propReader();
+		
+		String clientID = prop.getCilentID();
+		String brokerIP = prop.getBrokerIP();
+		String moduleURL = prop.getModuleURL();
+		
+		System.out.println(clientID);
+		System.out.println(brokerIP);
+		System.out.println(moduleURL);
+		
 	}
 
 }
