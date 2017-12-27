1da4bbd0f126d44d395834153a70f0dcfa7d6148
srsok <srsok3@gmail.com>
2017-12-27 13:16:57 +0900

    globalAiTES hello world

--- GlobalAiTESImpl/src/com/github/aites/globalaitesimpl/framework/Main.java
+++ GlobalAiTESImpl/src/com/github/aites/globalaitesimpl/framework/Main.java
@@ -3,5 +3,6 @@ package com.github.aites.globalaitesimpl.framework;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		System.out.println("");
 	}
 }
